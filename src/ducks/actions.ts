4344be09d41a6cd79e700ca501223293b471de07
John Gerald Agbayani <john.agbayani@idearobin.com>
2020-02-03 17:01:32 +0800

    Update action naming

--- src/ducks/actions.ts
+++ src/ducks/actions.ts
@@ -1,12 +1,12 @@
 import { Product } from '../types/product'
 import { Action, ActionType } from './types'
 
-export const addOneProduct = (product: Product): Action => ({
+export const addProduct = (product: Product): Action => ({
   type: ActionType.PRODUCTS_ADD,
   product,
 })
 
-export const removeOneProduct = (productId: string): Action => ({
+export const removeProduct = (productId: string): Action => ({
   type: ActionType.PRODUCTS_REMOVE,
   productId,
 })
