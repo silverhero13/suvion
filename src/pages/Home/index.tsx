23148b5995f49a8a38b015c8a46c4c5a869b48a9
John Gerald Agbayani <john.agbayani@idearobin.com>
2020-02-04 17:39:07 +0800

    Create homepage

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Recipe } from '../../types/recipe'
+import recipesApi from '../../api/recipes'
+
+const Home: React.FC = () => {
+  const [recipes, setRecipes] = React.useState<Recipe[]>([])
+
+  React.useEffect(() => {
+    recipesApi.list().then(x => {
+      setRecipes(x)
+    })
+  }, [])
+
+  return (
+    <div className="uk-section">
+      <div className="uk-container">
+        <h1 className="uk-heading-small">Recipes</h1>
+        <table className="uk-table uk-table-justify">
+          <thead>
+            <tr>
+              <th className="uk-width-1-3">Name</th>
+              <th>Cuisine</th>
+              <th className="uk-width-1-3">Description</th>
+              <th className="uk-text-right">Price</th>
+              <th className="uk-text-right">Discount</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {recipes.map(x => (
+              <tr key={x.id}>
+                <td>{x.name}</td>
+                <td>{x.cuisine}</td>
+                <td>{x.description}</td>
+                <td className="uk-text-right">{x.price}</td>
+                <td className="uk-text-right">{x.discount}</td>
+                <td>
+                  {x.isUnavailable ? <span uk-icon="icon: close" /> : null}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default Home
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Router as BrowserRouter, Route, Switch } from 'react-router-dom'
-import Page1 from '../pages/Page1'
-import Page2 from '../pages/Page2'
+import Home from '../pages/Home'
 import history from '../history'
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter history={history}>
       <Switch>
-        <Route path="/page1" component={Page1} exact />
-        <Route path="/page2" component={Page2} exact />
+        <Route path="/" component={Home} exact />
       </Switch>
     </BrowserRouter>
   )
