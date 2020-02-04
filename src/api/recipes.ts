504e6e6163b1dcdcfe2bfa8323428437a3f09eae
John Gerald Agbayani <john.agbayani@idearobin.com>
2020-02-04 16:14:18 +0800

    Add helper function to query recipes

---
+++ src/api/recipes.ts
@@ -0,0 +1,10 @@
+import Axios from 'axios'
+import { Recipe } from '../types/recipe'
+
+const list = async (): Promise<Recipe[]> => {
+  const response = await Axios.get<Recipe[]>('/recipes.json')
+
+  return response.data
+}
+
+export default { list }
